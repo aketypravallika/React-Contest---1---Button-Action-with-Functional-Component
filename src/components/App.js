ae92d6ca49608233864cccca868f04158d978565
pravallika <52813703+aketypravallika@users.noreply.github.com>
2020-11-06 22:29:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [click,setclicked]=react.useState(false);
+  const [click, setclicked] = useState(false);
   function handleClick(){
-  setclicked(true);
-  }
+    setclicked(true);
+  };
+
   return (
     <div id="main">
-     	<button id="click" onClick={handleClick}>
-		click
-		</button>
-    {
-    click &&(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
-    }
-    
+      <button id="click" onClick={() => handleClick()}>
+        click
+      </button>
+      {click && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
